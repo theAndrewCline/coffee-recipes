ea75bdf0fa140cac048829ce6c97693605e1a655
Andrew Cline <acline@precisionplanting.com>
2022-08-17 15:13:35 -0500

    feat: add sign in button

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,7 +1,9 @@
 describe('home page', () => {
-  it('passes', () => {
+  it('should ask you to sign in if there are no cookies', () => {
     cy.visit('/')
 
     cy.get('h1').should('have.text', 'Coffee Recipes')
+
+    cy.get('button').should('have.text', 'Sign In')
   })
 })
