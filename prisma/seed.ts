03a97382c62ca047ea6c732e0c861ae9f68243d9
Andrew Cline <acline@precisionplanting.com>
2022-08-17 13:20:58 -0500

    feat: update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,34 @@
 import { prisma } from '../db'
 import Chance from 'chance'
 
+const createStep = () => {
+  const chance = new Chance()
+  return {
+    title: chance.name(),
+    description: chance.name()
+  }
+}
+
+const createRecipe = () => {
+  const chance = new Chance()
+  return {
+    title: chance.name(),
+    steps: {
+      create: Array.from({ length: 3 }).map(createStep)
+    }
+  }
+}
+
 const seed = async () => {
   const users = Array.from({ length: 10 }).map(() => {
     const chance = new Chance()
 
     return {
       first_name: chance.first(),
-      last_name: chance.last()
+      last_name: chance.last(),
+      recipies: {
+        create: Array.from({ length: 3 }).map(createRecipe)
+      }
     }
   })
 
