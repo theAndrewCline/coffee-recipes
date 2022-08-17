bd1ea162fba079a094afc9be23d5249df089de92
Andrew Cline <acline@precisionplanting.com>
2022-08-17 14:16:24 -0500

    feat: finish seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,8 +4,7 @@ import Chance from 'chance'
 const createStep = () => {
   const chance = new Chance()
   return {
-    title: chance.name(),
-    description: chance.name()
+    title: chance.name()
   }
 }
 
@@ -19,23 +18,30 @@ const createRecipe = () => {
   }
 }
 
-const seed = async () => {
-  const users = Array.from({ length: 10 }).map(() => {
-    const chance = new Chance()
+const createUsers = Array.from({ length: 10 }).map(async () => {
+  const chance = new Chance()
 
-    return {
+  return prisma.user.create({
+    data: {
       first_name: chance.first(),
       last_name: chance.last(),
       recipies: {
         create: Array.from({ length: 3 }).map(createRecipe)
       }
+    },
+    include: {
+      recipies: {
+        include: {
+          steps: true
+        }
+      }
     }
   })
+})
 
+const seed = async () => {
   try {
-    await prisma.user.createMany({
-      data: users
-    })
+    await Promise.all(createUsers)
   } catch (e) {
     console.error(e)
   }
