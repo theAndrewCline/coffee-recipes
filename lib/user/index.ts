32b4948a8f461c6809b7d2edb8dc122ee1aae260
Andrew Cline <acline@precisionplanting.com>
2022-09-10 09:17:11 -0500

    feat: create initial create user function

---
+++ lib/user/index.ts
@@ -0,0 +1,78 @@
+import { DateTime } from 'luxon'
+import { DatabasePool, sql } from 'slonik'
+import { z } from 'zod'
+
+export const userSchema = z
+  .object({
+    id: z.number(),
+    name: z.string(),
+    email: z.string(),
+    email_verified: z.string()
+  })
+  .transform((u) => ({
+    id: u.id,
+    name: u.name,
+    email: u.email,
+    emailVerified: u.email_verified
+  }))
+
+type User = z.infer<typeof userSchema>
+
+export const createUserInputSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  emailVerified: z.string()
+})
+
+type UserInput = z.infer<typeof createUserInputSchema>
+
+export const makeUserFunctions = (pool: DatabasePool) => ({
+  async createUser(userInput: UserInput) {
+    const result = pool.one(sql.type(userSchema)`
+      INSERT INTO users (name, email, email_verified)
+      VALUES (${userInput.name}, ${userInput.email}, ${userInput.emailVerified})
+    `)
+
+    return result
+  }
+})
+
+if (import.meta.vitest) {
+  const { describe, it, expect, vi } = import.meta.vitest
+
+  describe('user functions', async () => {
+    const { createMockPool, createMockQueryResult } = await import('slonik')
+
+    it('createUser', async () => {
+      const emailVerified = DateTime.fromISO('2020-10-10').toSQL()
+
+      const query = vi.fn(async () =>
+        createMockQueryResult([
+          {
+            id: 24,
+            name: 'Andrew Cline',
+            email: 'andrew.cline77@gmail.com',
+            email_verified: emailVerified
+          }
+        ])
+      )
+
+      const pool = createMockPool({
+        query
+      })
+
+      const { createUser } = makeUserFunctions(pool)
+
+      const testUser = {
+        name: 'Andrew Cline',
+        email: 'andrew.cline77@gmail.com',
+        emailVerified
+      }
+
+      const result = await createUser(testUser)
+
+      expect(result).toMatchObject(testUser)
+      expect(result.id).toEqual(24)
+    })
+  })
+}
